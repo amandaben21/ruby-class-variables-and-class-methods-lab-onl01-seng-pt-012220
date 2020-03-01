607d633088bb38facdb3b670016191049ba5f5cb
Amanda Contreras <amandaben21@aol.com>
2020-03-01 23:57:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,9 @@
 class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
-  @@name= []
-  @@artists=[]
-  @@genres=[]
+  @@name = []
+  @@artists = []
+  @@genres = []
   
   def initialize(name, artist, genre)
     @@name = name
