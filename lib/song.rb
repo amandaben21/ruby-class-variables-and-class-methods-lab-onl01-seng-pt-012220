844eddbf35a2ccd701102e3593d9bb7719fcc06a
Amanda Contreras <amandaben21@aol.com>
2020-03-02 00:44:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,8 +29,9 @@ class Song
    genre_count = {}
    @@genre.each do |genre|
     if genre_count [genre]
-      genre_count [genre] +=1
-    else genre_count[genre] = 1
+       genre_count [genre] +=1
+    else 
+      genre_count[genre] = 1
     end
   end
   genre_count
